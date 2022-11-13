9dde98b80f32f98ec1e8a75c392a17669cde14ec
Williams Ajayi Balogun <williamsdev.official@gmail.com>
2022-11-13 14:57:32 +0100

    fix: edited styling to remove position error in the nav modal

--- src/routes/Root.js
+++ src/routes/Root.js
@@ -7,7 +7,7 @@ const Root = ({setShow}) => {
     <div className='flex justify-center items-center avg:w-3/4'>
         <nav className='w-full'>
             <ul className='flex flex-col avg:flex-row h-64 avg:h-auto justify-between avg:items-center avg:justify-evenly'>
-                <li className='flex avg:items-center justify-between h-full'>
+                <li className='flex avg:items-center justify-between avg:h-full'>
                     <Link onClick={()=>setShow(false)} className='link' to='/'>Home</Link>
                     <button onClick={()=>setShow(false)} className='text-3xl text-white avg:hidden'><IoClose /></button>
                 </li>
